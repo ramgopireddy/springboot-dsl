c36de23aa7c79d6e883a0394c877cee225f62024
Ram Gopireddy <rgopired@redhat.com>
2020-04-13 18:19:07 -0400

    test message

--- src/main/java/org/my/dev/KafkaRouteBuilder.java
+++ src/main/java/org/my/dev/KafkaRouteBuilder.java
@@ -9,17 +9,11 @@ public class KafkaRouteBuilder extends RouteBuilder {
     @Override
     public void configure() throws Exception {
 
-        from("direct:start")
-        .setBody(constant("Message from Camel"))          // Message to send
+        from("amq:incomingOrders")
+        .setBody(constant("Message from amq"))          // Message to send
         .setHeader(KafkaConstants.KEY, constant("Camel")) // Key of the message
-        .to("kafka:sampleTopic?brokers=localhost:9092");
-
-        from("kafka:sampleTopic?brokers=localhost:9092&groupId=1234")
-        .log("Message received from Kafka : ${body}")
-        .log("    on the topic ${headers[kafka.TOPIC]}")
-        .log("    on the partition ${headers[kafka.PARTITION]}")
-        .log("    with the offset ${headers[kafka.OFFSET]}")
-        .log("    with the key ${headers[kafka.KEY]}");
+        .log("${body}")
+        .to("kafka:incomingOrderskafka?brokers=fis-cluster-kafka-bootstrap:9092");
 
     }
 }
