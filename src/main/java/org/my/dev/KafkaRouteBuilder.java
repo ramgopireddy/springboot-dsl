a49435108602bf6fd252dedfa3d349cb1f14a9ac
Ram Gopireddy <rgopired@redhat.com>
2020-04-14 11:45:16 -0400

    updated properties

--- src/main/java/org/my/dev/KafkaRouteBuilder.java
+++ src/main/java/org/my/dev/KafkaRouteBuilder.java
@@ -7,20 +7,14 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaRouteBuilder extends RouteBuilder {
-    @Value("${kafka.topic.name}")
-    private String topicName;
-    
-    @Value("${kafka.bootstrap.url}")
-    private String bootstrapUrl;
-
     @Override
     public void configure() throws Exception {
 
         from("amq:incomingOrders?acknowledgementModeName=CLIENT_ACKNOWLEDGE")
         .setHeader(KafkaConstants.KEY, constant("Camel")) // Key of the message
-        .setHeader(KafkaConstants.TOPIC, constant(topicName)) // Topic Name
+        .setHeader(KafkaConstants.TOPIC, constant("incomingOrderskafka")) // Topic Name
         .log("${body}")
-        .to("kafka:${topicName}?brokers=${bootstrapUrl}&requestRequiredAcks=all&synchronous=true");
+        .to("kafka:incomingOrderskafka?brokers=fis-cluster-kafka-bootstrap:9092&requestRequiredAcks=all&synchronous=true");
 
     }
 }
