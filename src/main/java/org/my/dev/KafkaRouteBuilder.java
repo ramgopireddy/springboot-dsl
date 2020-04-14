7c3d38b69aa31498b0fe0eed8e0f649c8182807f
Ram Gopireddy <rgopired@redhat.com>
2020-04-14 11:36:44 -0400

    property updates

--- src/main/java/org/my/dev/KafkaRouteBuilder.java
+++ src/main/java/org/my/dev/KafkaRouteBuilder.java
@@ -2,19 +2,25 @@ package org.my.dev;
 
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kafka.KafkaConstants;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class KafkaRouteBuilder extends RouteBuilder {
+    @Value("${kafka.topic.name}")
+    private String topicName;
     
+    @Value("${kafka.bootstrap.url}")
+    private String bootstrapUrl;
+
     @Override
     public void configure() throws Exception {
 
         from("amq:incomingOrders?acknowledgementModeName=CLIENT_ACKNOWLEDGE")
         .setHeader(KafkaConstants.KEY, constant("Camel")) // Key of the message
-        .setHeader(KafkaConstants.TOPIC, constant("${{kafka.topic.name}}")) // Topic Name
+        .setHeader(KafkaConstants.TOPIC, constant(topicName)) // Topic Name
         .log("${body}")
-        .to("kafka:${{kafka.topic.name}}?brokers=${kafka.bootstrap.url}&requestRequiredAcks=all&synchronous=true");
+        .to("kafka:${topicName}?brokers=${bootstrapUrl}&requestRequiredAcks=all&synchronous=true");
 
     }
 }
